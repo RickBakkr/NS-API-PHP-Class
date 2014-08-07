3bad00aa3327a98e6e27a68dc25bcc33bf71f170
RickB2000 <rick.b@hotmail.com>
2014-08-07 18:42:40 +0200

    Create ns.class.php

---
+++ ns.class.php
@@ -0,0 +1,18 @@
+<?php
+/* 
+ * NS API Class
+ * @package NS-API-PHP-Class
+ * @author Rick Bakker / RickB2000 @ GitHub.
+ */
+ 
+Class NS {
+
+  private $username;
+  private $password;
+
+  public function __construct($username, $password) {
+    $this->username = $username;
+    $this->password = $password;
+  }
+
+}
